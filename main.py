5f9ee6ea755e4a12c02981e63310209daeda3a4f
amarildokola <amarildo.kola@kulmai.com>
2026-03-11 16:04:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World from E2E test!"
+    return "Hello World from E2E test! asdsasadasdasdasdasdasdasdasd"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
